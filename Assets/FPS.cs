fa265d511d7c55b95b337ac6c5d4e9986a900111
Javier Ron <javierron90@gmail.com>
2020-04-26 13:09:53 +0200

    Initial commit

---
+++ Assets/FPS.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.VFX;
+
+public class FPS : MonoBehaviour
+{
+    public Text ms;
+    public Text fps;
+    public Text rate;
+    public Text particles;
+    public Slider slider;
+    public VisualEffect effect;
+
+    void Start(){
+        Application.targetFrameRate = 300;
+    }
+
+    void Update()
+    {
+        ms.text = (Time.deltaTime * 1000f).ToString("000.00") + " ms";
+        fps.text = (1f / Time.deltaTime).ToString("00.00") + " fps";
+        rate.text = slider.value.ToString() + " particles/sec";
+        particles.text = effect.aliveParticleCount.ToString() + " total particles";
+    }
+}
---
+++ Assets/VFXController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.UI;
+
+public class VFXController : MonoBehaviour
+{
+
+    public Slider slider;
+    public VisualEffect effect;	
+
+    public void ApplyUpdate()
+    {
+        effect.SetInt("rate", (int)slider.value);   
+    }
+}
